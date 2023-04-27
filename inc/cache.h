fcb3edbeacb28870d57d69ad79c88ca507e98ce4
gargi81202 <64091977+gargi81202@users.noreply.github.com>
2023-04-27 10:34:51 +0530

    Add files via upload

---
+++ inc/cache.h
@@ -0,0 +1,290 @@
+#ifndef CACHE_H
+#define CACHE_H
+#include <bits/stdc++.h>
+#include "memory_class.h"
+#include <cstdint>
+// PAGE
+extern uint32_t PAGE_TABLE_LATENCY, SWAP_LATENCY;
+
+// CACHE TYPE
+#define IS_ITLB 0
+#define IS_DTLB 1
+#define IS_STLB 2
+#define IS_L1I  3
+#define IS_L1D  4
+#define IS_L2C  5
+#define IS_LLC  6
+
+// INSTRUCTION TLB
+#define ITLB_SET 16
+#define ITLB_WAY 4
+#define ITLB_RQ_SIZE 16
+#define ITLB_WQ_SIZE 16
+#define ITLB_PQ_SIZE 0
+#define ITLB_MSHR_SIZE 8
+#define ITLB_LATENCY 1
+
+// DATA TLB
+#define DTLB_SET 16
+#define DTLB_WAY 4
+#define DTLB_RQ_SIZE 16
+#define DTLB_WQ_SIZE 16
+#define DTLB_PQ_SIZE 0
+#define DTLB_MSHR_SIZE 8
+#define DTLB_LATENCY 1
+
+// SECOND LEVEL TLB
+#define STLB_SET 128
+#define STLB_WAY 12
+#define STLB_RQ_SIZE 32
+#define STLB_WQ_SIZE 32
+#define STLB_PQ_SIZE 0
+#define STLB_MSHR_SIZE 16
+#define STLB_LATENCY 8
+
+// L1 INSTRUCTION CACHE
+#define L1I_SET 64
+#define L1I_WAY 8
+#define L1I_RQ_SIZE 64
+#define L1I_WQ_SIZE 64
+#define L1I_PQ_SIZE 32
+#define L1I_MSHR_SIZE 8/2
+#define L1I_LATENCY 4
+
+// L1 DATA CACHE
+#define L1D_SET 64
+#define L1D_WAY 12
+#define L1D_RQ_SIZE 64
+#define L1D_WQ_SIZE 64
+#define L1D_PQ_SIZE 8
+#define L1D_MSHR_SIZE 16/2
+#define L1D_LATENCY 5
+
+// L2 CACHE
+#define L2C_SET 1024
+#define L2C_WAY 8
+#define L2C_RQ_SIZE 32
+#define L2C_WQ_SIZE 32
+#define L2C_PQ_SIZE 16
+#define L2C_MSHR_SIZE 32/2
+#define L2C_LATENCY 10  // 4/5 (L1I or L1D) + 10 = 14/15 cycles
+
+// LAST LEVEL CACHE
+#define LLC_SET NUM_CPUS*2048
+#define LLC_WAY 16
+#define LLC_RQ_SIZE NUM_CPUS*L2C_MSHR_SIZE //48
+#define LLC_WQ_SIZE NUM_CPUS*L2C_MSHR_SIZE //48
+#define LLC_PQ_SIZE NUM_CPUS*32
+#define LLC_MSHR_SIZE NUM_CPUS*64/2
+#define LLC_LATENCY 20  // 4/5 (L1I or L1D) + 10 + 20 = 34/35 cycles
+
+#define ROUND_MAX 100
+#define SCORE_MAX 31
+#define TABLE_SIZE 64
+#define QUEUE_SIZE 15
+#define BAD_SCORE 10
+#define BW 16
+#define LOW_SCORE 20
+#define upper_limit_gauge 1000
+#define lower_limit_gauge 10
+
+class CACHE : public MEMORY {
+  public:
+    uint32_t cpu;
+    const string NAME;
+    const uint32_t NUM_SET, NUM_WAY, NUM_LINE, WQ_SIZE, RQ_SIZE, PQ_SIZE, MSHR_SIZE;
+    uint32_t LATENCY;
+    BLOCK **block;
+    int fill_level;
+    uint32_t MAX_READ, MAX_FILL;
+    uint32_t reads_available_this_cycle;
+    uint8_t cache_type;
+    int offset;
+    int offsets[46] ;
+    std::map<int, int> offsetAndScore;
+ 
+    uint32_t round ;
+    uint8_t prefetch;
+
+    uint32_t curr_llc, prev_llc, gauge, rate;
+    uint32_t MSHR_thresh;
+   
+    std::unordered_set<uint64_t> left;
+    std::unordered_set<uint64_t> right;
+    std::map<uint64_t, uint32_t> QUEUE;
+    // prefetch stats
+    uint64_t pf_requested,
+             pf_issued,
+             pf_useful,
+             pf_useless,
+             pf_fill;
+
+    // queues
+    PACKET_QUEUE WQ{NAME + "_WQ", WQ_SIZE}, // write queue
+                 RQ{NAME + "_RQ", RQ_SIZE}, // read queue
+                 PQ{NAME + "_PQ", PQ_SIZE}, // prefetch queue
+                 MSHR{NAME + "_MSHR", MSHR_SIZE}, // MSHR
+                 PROCESSED{NAME + "_PROCESSED", ROB_SIZE}; // processed queue
+
+    uint64_t sim_access[NUM_CPUS][NUM_TYPES],
+             sim_hit[NUM_CPUS][NUM_TYPES],
+             sim_miss[NUM_CPUS][NUM_TYPES],
+             roi_access[NUM_CPUS][NUM_TYPES],
+             roi_hit[NUM_CPUS][NUM_TYPES],
+             roi_miss[NUM_CPUS][NUM_TYPES];
+
+    uint64_t total_miss_latency;
+
+    // constructor
+    CACHE(string v1, uint32_t v2, int v3, uint32_t v4, uint32_t v5, uint32_t v6, uint32_t v7, uint32_t v8)
+        : NAME(v1), NUM_SET(v2), NUM_WAY(v3), NUM_LINE(v4), WQ_SIZE(v5), RQ_SIZE(v6), PQ_SIZE(v7), MSHR_SIZE(v8) {
+        
+        LATENCY = 0;
+	
+        int temp[] = {1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36, 40, 45, 48, -1, -2, -3, -4, -5, -6, -8, -9, -10, -12, -15, -16, -18, -20, -24, -25, -27, -30, -32, -36, -40, -45, -48};        // ADDED BY US
+        for(int i=0; i<46; i++){
+		offsets[i] = temp[i];
+	}
+	round = 0;
+	prefetch = 1;
+	curr_llc = rdtsc();
+	prev_llc = rdtsc();
+	gauge = 0;
+	rate = 0;
+	MSHR_thresh = 0;
+	offset=1;
+        // cache block
+        block = new BLOCK* [NUM_SET];
+        for (uint32_t i=0; i<NUM_SET; i++) {
+            block[i] = new BLOCK[NUM_WAY];
+
+            for (uint32_t j=0; j<NUM_WAY; j++) {
+                block[i][j].lru = j;
+                block[i][j].freq = 0;
+                block[i][j].counter = j;
+            }
+        }
+
+        for (uint32_t i=0; i<NUM_CPUS; i++) {
+            upper_level_icache[i] = NULL;
+            upper_level_dcache[i] = NULL;
+
+            for (uint32_t j=0; j<NUM_TYPES; j++) {
+                sim_access[i][j] = 0;
+                sim_hit[i][j] = 0;
+                sim_miss[i][j] = 0;
+                roi_access[i][j] = 0;
+                roi_hit[i][j] = 0;
+                roi_miss[i][j] = 0;
+            }
+        }
+
+        total_miss_latency = 0;
+
+        lower_level = NULL;
+        extra_interface = NULL;
+        fill_level = -1;
+        MAX_READ = 1;
+        MAX_FILL = 1;
+
+        pf_requested = 0;
+        pf_issued = 0;
+        pf_useful = 0;
+        pf_useless = 0;
+        pf_fill = 0;
+
+        for (int i = 0; i < 46; i++)
+        {
+            offsetAndScore[offsets[i]] = 0;
+        }
+    };
+
+    // destructor
+    ~CACHE() {
+        for (uint32_t i=0; i<NUM_SET; i++)
+            delete[] block[i];
+        delete[] block;
+    };
+
+    // functions
+    inline uint64_t rdtsc() {
+        uint32_t lo, hi;
+        __asm__ __volatile__ (
+            "rdtsc" : "=a" (lo), "=d" (hi)
+        );
+        return ((uint64_t)hi << 32) | lo;
+    }
+    int  add_rq(PACKET *packet),
+         add_wq(PACKET *packet),
+         add_pq(PACKET *packet);
+
+    void remove_line(uint64_t full_addr);
+
+    void return_data(PACKET *packet),
+         operate(),
+         increment_WQ_FULL(uint64_t address);
+
+    uint32_t get_occupancy(uint8_t queue_type, uint64_t address),
+             get_size(uint8_t queue_type, uint64_t address);
+
+    int  check_hit(PACKET *packet),
+         invalidate_entry(uint64_t inval_addr),
+         check_mshr(PACKET *packet),
+         prefetch_line(uint64_t ip, uint64_t base_addr, uint64_t pf_addr, int prefetch_fill_level, uint32_t prefetch_metadata),
+         kpc_prefetch_line(uint64_t base_addr, uint64_t pf_addr, int prefetch_fill_level, int delta, int depth, int signature, int confidence, uint32_t prefetch_metadata);
+
+    void handle_fill(),
+         handle_writeback(),
+         handle_read(),
+         handle_prefetch();
+
+
+    void add_mshr(PACKET *packet),
+         update_fill_cycle(),
+         llc_initialize_replacement(),
+         update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit),
+         llc_update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit),
+         lru_update(uint32_t set, uint32_t way),
+         lfu_update(uint32_t set, uint32_t way, uint8_t hit),    // ADDED BY US
+         fifo_update(uint32_t set, uint32_t way, uint8_t hit),    // ADDED BY US
+         rand_update(uint32_t set, uint32_t way),    // ADDED BY US
+         fill_cache(uint32_t set, uint32_t way, PACKET *packet),
+         replacement_final_stats(),
+         llc_replacement_final_stats(),
+         //prefetcher_initialize(),
+         l1d_prefetcher_initialize(),
+         l2c_prefetcher_initialize(),
+         llc_prefetcher_initialize(),
+         prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type),
+         l1d_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type),
+         prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr),
+         l1d_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in),
+         //prefetcher_final_stats(),
+         l1d_prefetcher_final_stats(),
+         l2c_prefetcher_final_stats(),
+         llc_prefetcher_final_stats();
+    void (*l1i_prefetcher_cache_operate)(uint32_t, uint64_t, uint8_t, uint8_t);
+    void (*l1i_prefetcher_cache_fill)(uint32_t, uint64_t, uint32_t, uint32_t, uint8_t, uint64_t);
+
+    uint32_t l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in),
+         llc_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in),
+         l2c_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in),
+         llc_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr, uint32_t metadata_in);
+
+
+   // uint32_t l2c_BOprefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type, uint32_t metadata_in, uint32_t offset);
+
+    int l2c_BO_get_offset();
+
+    uint32_t get_set(uint64_t address),
+             get_way(uint64_t address, uint32_t set),
+             find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type),
+             llc_find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type),
+             lru_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type),
+             lfu_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type), // ADDED BY US
+             fifo_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type), // ADDED BY US
+             rand_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type); // ADDED BY US
+
+};
+
+#endif
---
+++ replacement/base_replacement.cc
@@ -0,0 +1,233 @@
+#include "cache.h"
+#include <cstdlib>
+
+//void CACHE::update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit)
+//{
+  //  if (type == WRITEBACK) {
+    //    if (hit) // wrietback hit does not update LRU state
+      //      return;
+//    }
+
+  //  return lru_update(set, way);
+//}
+
+uint32_t CACHE::rand_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
+{
+    uint32_t way = 0;
+
+    // fill invalid line first
+    for (way=0; way<NUM_WAY; way++) {
+        if (block[set][way].valid == false) {
+
+            DP ( if (warmup_complete[cpu]) {
+            cout << "[" << NAME << "] " << __func__ << " instr_id: " << instr_id << " invalid set: " << set << " way: " << way;
+            cout << hex << " address: " << (full_addr>>LOG2_BLOCK_SIZE) << " victim address: " << block[set][way].address << " data: " << block[set][way].data;
+            cout << dec << " lru: " << block[set][way].lru << endl; });
+
+            break;
+        }
+    }
+
+    // RAND victim
+    if (way == NUM_WAY) {
+        way = rand() % NUM_WAY;
+    }
+
+
+    return way;
+}
+
+
+uint32_t CACHE::lfu_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
+{
+    uint32_t way = 0;
+
+    // fill invalid line first
+    for (way=0; way<NUM_WAY; way++) {
+        if (block[set][way].valid == false) {
+
+            DP ( if (warmup_complete[cpu]) {
+            cout << "[" << NAME << "] " << __func__ << " instr_id: " << instr_id << " invalid set: " << set << " way: " << way;
+            cout << hex << " address: " << (full_addr>>LOG2_BLOCK_SIZE) << " victim address: " << block[set][way].address << " data: " << block[set][way].data;
+            cout << dec << " lru: " << block[set][way].lru << endl; });
+
+            break;
+        }
+    }
+
+
+
+    // LFU victim
+    uint32_t minway = way;
+    if (way == NUM_WAY) {
+        minway = 0;
+        uint32_t minfreq = block[set][0].freq;
+        for (way=0; way<NUM_WAY; way++) {
+            if (block[set][way].freq < minfreq) {
+
+                minway = way;
+                minfreq = block[set][way].freq;
+
+                DP ( if (warmup_complete[cpu]) {
+                cout << "[" << NAME << "] " << __func__ << " instr_id: " << instr_id << " replace set: " << set << " way: " << way;
+                cout << hex << " address: " << (full_addr>>LOG2_BLOCK_SIZE) << " victim address: " << block[set][way].address << " data: " << block[set][way].data;
+                cout << dec << " lru: " << block[set][way].lru << endl; });
+
+                // break;
+            }
+        }
+    }
+
+
+    return minway;
+}
+
+uint32_t CACHE::fifo_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
+{
+    uint32_t way = 0;
+     
+    // fill invalid line first
+    for (way=0; way<NUM_WAY; way++) {
+        if (block[set][way].valid == false) {
+
+            DP ( if (warmup_complete[cpu]) {
+            cout << "[" << NAME << "] " << __func__ << " instr_id: " << instr_id << " invalid set: " << set << " way: " << way;
+            cout << hex << " address: " << (full_addr>>LOG2_BLOCK_SIZE) << " victim address: " << block[set][way].address << " data: " << block[set][way].data;
+            cout << dec << " lru: " << block[set][way].lru << endl; });
+
+            break;
+        }
+    }
+
+    // LRU victim
+    if (way == NUM_WAY) {
+        for (way=0; way<NUM_WAY; way++) {
+            if (block[set][way].counter == 0) {
+
+                DP ( if (warmup_complete[cpu]) {
+                cout << "[" << NAME << "] " << __func__ << " instr_id: " << instr_id << " replace set: " << set << " way: " << way;
+                cout << hex << " address: " << (full_addr>>LOG2_BLOCK_SIZE) << " victim address: " << block[set][way].address << " data: " << block[set][way].data;
+                cout << dec << " fifo: " << block[set][way].lru << endl; });
+
+                break;
+            }
+        }
+    }
+
+    if (way == NUM_WAY) {
+        cerr << "[" << NAME << "] " << __func__ << " no victim! set: " << set << endl;
+        assert(0);
+    }
+
+    return way;
+}
+
+
+uint32_t CACHE::lru_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
+{
+    uint32_t way = 0;
+
+    // fill invalid line first
+    for (way=0; way<NUM_WAY; way++) {
+        if (block[set][way].valid == false) {
+
+            DP ( if (warmup_complete[cpu]) {
+            cout << "[" << NAME << "] " << __func__ << " instr_id: " << instr_id << " invalid set: " << set << " way: " << way;
+            cout << hex << " address: " << (full_addr>>LOG2_BLOCK_SIZE) << " victim address: " << block[set][way].address << " data: " << block[set][way].data;
+            cout << dec << " lru: " << block[set][way].lru << endl; });
+
+            break;
+        }
+    }
+
+    // LRU victim
+    if (way == NUM_WAY) {
+        for (way=0; way<NUM_WAY; way++) {
+            if (block[set][way].lru == NUM_WAY-1) {
+
+                DP ( if (warmup_complete[cpu]) {
+                cout << "[" << NAME << "] " << __func__ << " instr_id: " << instr_id << " replace set: " << set << " way: " << way;
+                cout << hex << " address: " << (full_addr>>LOG2_BLOCK_SIZE) << " victim address: " << block[set][way].address << " data: " << block[set][way].data;
+                cout << dec << " lru: " << block[set][way].lru << endl; });
+
+                break;
+            }
+        }
+    }
+
+    if (way == NUM_WAY) {
+        cerr << "[" << NAME << "] " << __func__ << " no victim! set: " << set << endl;
+        assert(0);
+    }
+
+    return way;
+}
+
+void CACHE::lru_update(uint32_t set, uint32_t way)
+{
+    // update lru replacement state
+    for (uint32_t i=0; i<NUM_WAY; i++) {
+        if (block[set][i].lru < block[set][way].lru) {
+            block[set][i].lru++;
+        }
+    }
+    block[set][way].lru = 0; // promote to the MRU position
+}
+
+void CACHE::lfu_update(uint32_t set, uint32_t way, uint8_t hit)
+{
+    if (hit) block[set][way].freq++;
+    else block[set][way].freq = 0; // promote to the MRU position
+    return;
+
+}
+
+
+void CACHE::fifo_update(uint32_t set, uint32_t way, uint8_t hit)
+{
+    if (!hit)
+    {
+        for (uint32_t i = 0; i < NUM_WAY; i++)
+        {
+            if (i == way) block[set][way].counter = NUM_WAY - 1;
+            else block[set][i].counter--;
+        }
+        
+    }
+}
+void CACHE::rand_update(uint32_t set, uint32_t way)
+{
+    return;
+}
+
+void CACHE::replacement_final_stats()
+{
+
+}
+
+#ifdef NO_CRC2_COMPILE
+void InitReplacementState()
+{
+
+}
+
+uint32_t GetVictimInSet (uint32_t cpu, uint32_t set, const BLOCK *current_set, uint64_t PC, uint64_t paddr, uint32_t type)
+{
+    return 0;
+}
+
+void UpdateReplacementState (uint32_t cpu, uint32_t set, uint32_t way, uint64_t paddr, uint64_t PC, uint64_t victim_addr, uint32_t type, uint8_t hit)
+{
+
+}
+
+void PrintStats_Heartbeat()
+{
+
+}
+
+void PrintStats()
+{
+
+}
+#endif
